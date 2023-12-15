6ba7137588983b29cbd732bc1061edfb38ccc0ba
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2023-12-16 00:35:03 +0100

    neo2: Change BIOS loading

--- include/bus/bus.hh
+++ include/bus/bus.hh
@@ -10,7 +10,7 @@ class Bus
   public:
     Bus();
 
-    std::vector<std::uint8_t> memory_map;
+    std::vector<std::uint8_t> bios;
 
     void load_bios(const std::string &bios_path);
     std::uint8_t read(uint32_t address);
