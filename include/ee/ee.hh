f2ebe8853295d2bc4593dde242d5c688f910a8ca
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2023-12-21 12:59:07 +0100

    ee: Add the foundations of the core (GPRs and COP0 Registers)

--- include/ee/ee.hh
+++ include/ee/ee.hh
@@ -12,6 +12,14 @@ enum class EmulationMode
     DynamicRecompiler
 };
 
+union uint128_t {
+    unsigned __int128 u128;
+    std::uint64_t u64[2];
+    std::uint32_t u32[4];
+    std::uint16_t u16[8];
+    std::uint8_t u8[16];
+};
+
 class EE
 {
   private:
@@ -24,5 +32,10 @@ class EE
     void run();
 
     Bus *bus;
+
     std::uint32_t pc;
+    std::uint32_t next_pc;
+
+    uint128_t registers[32];
+    std::uint32_t cop0_registers[32];
 };
