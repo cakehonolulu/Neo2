cc21b17ae1ce009709fd573f79bfdefc8e7c2a1f
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2023-12-21 12:27:56 +0100

    bus: Make Bus class accept more-than-1 impl.

--- include/bus/bus.hh
+++ include/bus/bus.hh
@@ -1,14 +1,21 @@
 #pragma once
 
 #include <cstdint>
+#include <functional>
 #include <string>
 #include <vector>
 
+enum class BusMode
+{
+    Ranged,
+    SoftwareFastMem
+};
+
 class Bus
 {
-
+  private:
   public:
-    Bus();
+    Bus(BusMode mode = BusMode::SoftwareFastMem);
 
     std::vector<std::uint8_t> bios;
     std::vector<std::uint8_t> ram;
@@ -16,6 +23,8 @@ class Bus
     uintptr_t *address_space_r;
     uintptr_t *address_space_w;
 
+    std::function<std::uint32_t(std::uint32_t)> read32;
     void load_bios(const std::string &bios_path);
-    std::uint32_t read32(uint32_t address);
+    void fmem_init();
+    std::uint32_t fmem_read32(uint32_t address);
 };
