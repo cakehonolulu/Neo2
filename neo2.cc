48c14258fc35835fee522ae298715061fdd826e4
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2024-10-22 15:53:14 +0200

    neo2: Better banner

--- neo2.cc
+++ neo2.cc
@@ -1,12 +1,50 @@
 #include <neo2.hh>
+#include <sstream>
+
+std::string colorCode(int r, int g, int b) {
+    return "\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m";
+}
+
+std::string backgroundColorCode(int r, int g, int b) {
+    return "\033[48;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m";
+}
+
+std::string resetColor() {
+    return "\033[0m";
+}
 
 Neo2::Neo2() : bus(BusMode::SoftwareFastMem), ee(&bus, EmulationMode::Interpreter)
 {
-	Logger::raw("  _   _           ____  \n"
-				" | \\ | | ___  ___|___ \\ \n"
-				" |  \\| |/ _ \\/ _ \\ __) |\n"
-				" | |\\  |  __/ (_) / __/ \n"
-				" |_| \\_|\\___|\\___/_____|   -   Simple Sony PlayStation 2 Emulator\n");
+    std::string banner =
+        "  _   _           ____  \n"
+        " | \\ | | ___  ___|___ \\ \n"
+        " |  \\| |/ _ \\/ _ \\ __) |\n"
+        " | |\\  |  __/ (_) / __/ \n"
+        " |_| \\_|\\___|\\___/_____|  -  Simple Sony PlayStation 2 Emulator\n";
+
+    int r_start = 48;
+    int g_start = 78;
+    int b_start = 151;
+
+    int r_end = 0;
+    int g_end = 173;
+    int b_end = 204;
+
+    int lines = 5;
+
+    std::istringstream bannerStream(banner);
+    std::string line;
+    int line_index = 0;
+
+    while (std::getline(bannerStream, line)) {
+        float ratio = (float)line_index / (lines - 1);
+        int r = r_start + static_cast<int>((r_end - r_start) * ratio);
+        int g = g_start + static_cast<int>((g_end - g_start) * ratio);
+        int b = b_start + static_cast<int>((b_end - b_start) * ratio);
+
+        Logger::raw("\033[1m" + backgroundColorCode(r, g, b) + colorCode(255, 255, 255) + line + resetColor() + "\n");
+        line_index++;
+    }
 }
 
 Neo2::~Neo2()
